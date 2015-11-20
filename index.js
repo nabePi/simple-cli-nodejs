ef8ead04e0a3a3981e072131db3fe7daf2f8c7e8
nabePi <bps.phi@gmail.com>
2015-11-20 20:19:26 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+var chalk = require('chalk');
+var request = require('superagent');
+var co = require('co');
+var prompt = require('co-prompt');
+var program = require('commander');
+
+program
+    .arguments('<file>')
+    .option('-u, --username <username>', 'The user to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function(file){
+        co(function *(){
+            var username = yield prompt('username:');
+            var password = yield prompt.password('password:');
+            request
+                .post('https://api.bitbucket.org/2.0/snippets')
+                .auth(username, password)
+                .attach('file', file)
+                .set('Accept', 'application/json')
+                .end(function(err, res){
+                    if(!err && res.ok){
+                        var link = res.body.links.html.href;
+                        console.log(chalk.bold.cyan('Snippet created: ') + link);
+                        process.exit(0);
+                    }
+
+                    var errorMessage;
+                    if(res && res.status === 401){
+                        errorMessage = "Authentication failed! Bad username/password?";
+                    }
+                    else if(err){
+                        errorMessage = err;
+                    }
+                    else{
+                        errorMessage = res.text;
+                    }
+                    console.error(chalk.red(errorMessage));
+                    process.exit(0);
+                });
+        });
+    })
+    .parse(process.argv);
